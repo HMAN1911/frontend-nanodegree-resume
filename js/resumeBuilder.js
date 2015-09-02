3df7b1718ac32ec99de23680064270a46efc4ab5
Harry Aydin <aydin.harun.23@gmail.com>
2015-09-02 22:22:41 +1000

    resumebuilder.js is ready for lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,69 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-$("#main").append('<img src="images/fry.jpg" alt=""/>');
-//wow so much fun
+var bio = {
+	"name": "Harun Aydin",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "0433 424 321",
+		"email": "aydin.harun.23@gmail.com",
+		"github": "https://github.com/HMAN1911",
+		"twitter": "@twitter",
+		"location": "Melbourne",
+		"welcomeMessage": "Hello there!"
+	},
+	"skills": [
+		"Design", "Web Development", "Drawing"
+	],
+	"biopic": "images/fry.jpg",
+	"display": "TODO some function goes here"
+};
+
+
+var education = {
+	"schools": [
+		{
+			"name": "Latrobe University",
+			"location": "Melbourne, Australia",
+			"degree": "Bachelor of Commerce / Arts",
+			"majors": [
+				"E-commerce", "Philosophy"
+			],
+			"dates": 23052010,
+			"url": "www.latrobe.edu.au"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front End Wed Development",
+			"school": "Udacity",
+			"date": 12122015,
+			"url": "www.udacity.com",
+			"display": "TODO some function goes here"
+		}
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Powercor Australia",
+			"title": "Ecommerce Analyst",
+			"location": "Melbourne",
+			"dates": "5-5-2009",
+			"description": "Ecommerce Analyst"
+		}
+	],
+	"display": "TODO Some function goes here"
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "Project 1",
+			"dates": "02072015",
+			"description": "Project 1",
+			"images": [
+				"www.google.com", "www.google.com"
+			],
+			"display": "TODO some function goes here"
+		}
+	]
+};
