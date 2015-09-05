98a6f88cb973b8485a6ced9d66073b7463282c26
Harry Aydin <aydin.harun.23@gmail.com>
2015-09-05 17:06:10 +1000

    resumebuilder.js is ready for lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -47,8 +47,8 @@ var work = {
 			"employer": "Powercor Australia",
 			"title": "Ecommerce Analyst",
 			"location": "Melbourne",
-			"dates": "5-5-2009",
-			"description": "Ecommerce Analyst"
+			"dates": "552009",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut magna ac lacus pellentesque lobortis ut nec ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quis elit massa. Mauris euismod velit eget aliquet ultricies. Maecenas lobortis ullamcorper orci, blandit ultricies ante tincidunt sed. Maecenas dignissim tellus dolor, non efficitur sem vehicula non. Nullam sodales ante vel elit ultricies laoreet. Curabitur sed ligula eu nulla facilisis porttitor et in ex. Aenean libero justo, condimentum at erat id, tristique efficitur tortor. Vivamus lectus urna, feugiat at venenatis id, finibus eget lectus. Pellentesque finibus eget lectus convallis scelerisque. Aenean sit amet ullamcorper libero. Donec ullamcorper tempor augue, non vehicula diam sodales nec. Morbi a eleifend lorem."
 		}
 	],
 	"display": "TODO Some function goes here"
@@ -59,11 +59,49 @@ var projects = {
 		{
 			"title": "Project 1",
 			"dates": "02072015",
-			"description": "Project 1",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut magna ac lacus pellentesque lobortis ut nec ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quis elit massa. Mauris euismod velit eget aliquet ultricies. Maecenas lobortis ullamcorper orci, blandit ultricies ante tincidunt sed. Maecenas dignissim tellus dolor, non efficitur sem vehicula non. Nullam sodales ante vel elit ultricies laoreet. Curabitur sed ligula eu nulla facilisis porttitor et in ex. Aenean libero justo, condimentum at erat id, tristique efficitur tortor. Vivamus lectus urna, feugiat at venenatis id, finibus eget lectus. Pellentesque finibus eget lectus convallis scelerisque. Aenean sit amet ullamcorper libero. Donec ullamcorper tempor augue, non vehicula diam sodales nec. Morbi a eleifend lorem.",
 			"images": [
-				"www.google.com", "www.google.com"
+				"images/197x148.gif", "images/197x148.gif"
 			],
 			"display": "TODO some function goes here"
 		}
 	]
 };
+
+$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+
+//bio section
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+//$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+//$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.contacts.welcomeMessage));
+$("#header").append(HTMLskillsStart);
+$("#header").append(HTMLskills.replace("%data%", bio.skills[0]));
+$("#header").append(HTMLskills.replace("%data%", bio.skills[1]));
+$("#header").append(HTMLskills.replace("%data%", bio.skills[2]));
+
+//job 1
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(HTMLworkEmployer.replace("%data%", work.jobs[0].employer + HTMLworkTitle.replace("%data%", work.jobs[0].title)));
+$(".work-entry").append(HTMLworkDates.replace("%data%", work.jobs[0].dates));
+$(".work-entry").append(HTMLworkLocation.replace("%data%", work.jobs[0].location));
+$(".work-entry").append(HTMLworkDescription.replace("%data%", work.jobs[0].description));
+
+//job 2
+//$("#workExperience").prepend(HTMLworkStart);
+//$("#workExperience").append(HTMLworkEmployer.replace("%data%", work.jobs[1].employer + HTMLworkTitle.replace("%data%", work.jobs[0].title)));
+//$("#workExperience").append(HTMLworkDates.replace("%data%", work.jobs[1].dates));
+//$("#workExperience").append(HTMLworkLocation.replace("%data%", work.jobs[1].location));
+//$("#workExperience").append(HTMLworkDescription.replace("%data%", work.jobs[1].description));
+
+//project 1
+$("#projects").append(HTMLprojectStart);
+$(".project-entry").append(HTMLprojectTitle.replace("%data%", projects.projects[0].title));
+$(".project-entry").append(HTMLprojectDates.replace("%data%", projects.projects[0].dates));
+$(".project-entry").append(HTMLprojectDescription.replace("%data%", projects.projects[0].description));
+$(".project-entry").append(HTMLprojectImage.replace("%data%", projects.projects[0].images[0]));
+$(".project-entry").append(HTMLprojectImage.replace("%data%", projects.projects[0].images[1]));
